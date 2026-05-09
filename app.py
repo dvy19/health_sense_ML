d0f517d0b65da59bdd902bfd0271a31fb7168ab3
dvy19 <divyadixit.311204@gmail.com>
2026-05-10 02:48:43 +0530

    setup

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+from routes.disease_routes import disease_bp
+app = Flask(__name__)
+app.register_blueprint(disease_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ services/diseases_service.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv("indian_diseases_dataset.csv")
+
+def get_top_5_diseases_by_state(state_name):
+    state_df = df[df['state'].str.lower() == state_name.lower()]
+    if state_df.empty:
+        return {"error": f"No data found for state: {state_name}"}
+    return state_df['disease_name'].value_counts().head(5).to_dict()
+
+def get_top_5_diseases_by_city(city_name):
+    city_df = df[df['city'].str.lower() == city_name.lower()]
+    if city_df.empty:
+        return {"error": f"No data found for city: {city_name}"}
+    return city_df['disease_name'].value_counts().head(5).to_dict()
